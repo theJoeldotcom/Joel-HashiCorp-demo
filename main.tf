d0a684552a5d8388ffee44dbe3a0f04478144b17
thejoeldotcom <thejoel@thejoel.com>
2025-04-11 18:27:11 -0700

    adding EC2

--- main.tf
+++ main.tf
@@ -98,3 +98,14 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+#EC2 instances
+# ami-0a9841b43a830391e
+
+resource "aws_instance" "webserver" {
+  ami = "ami-0a9841b43a830391e"
+  instance_type = "t3a.small"
+  key_name = "AWS-dev"
+  subnet_id = "subnet-07090382ff9e09e31"
+  vpc_security_group_ids = "sg-011b08407653bde31"
+}
