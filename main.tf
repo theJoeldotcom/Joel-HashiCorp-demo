eb393d20a819a83da2872572b8f333abb4322bd5
thejoeldotcom <thejoel@thejoel.com>
2025-04-12 12:26:53 -0700

    ELB

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_lb" "ELB-public" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            = aws_subnet.public.id
 
   enable_deletion_protection = true
 }
