bb65e6837ab9a305911bc2ebef45ac787fe58b1f
theJoeldotcom <theJoel@theJoel.com>
2025-04-12 19:08:20 -0700

    Add files via upload

--- main.tf
+++ main.tf
@@ -176,12 +176,12 @@ resource "aws_lb_target_group" "webservers" {
 
 resource "aws_lb_target_group_attachment" "target-group-attachment" {
   target_group_arn = aws_lb_target_group.webservers.arn
-  target_id        = [aws_instance.webserver.id, aws_instance.webserver2.id]
+  target_id        = [aws_instance.webserver.id]
   port             = 80
 }
 
-#resource "aws_lb_target_group_attachment" "target-group-attachment2" {
-#  target_group_arn = aws_lb_target_group.webservers.arn
-#  target_id        = aws_instance.webserver2.id
-#  port             = 80
-#}
+resource "aws_lb_target_group_attachment" "target-group-attachment2" {
+  target_group_arn = aws_lb_target_group.webservers.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
